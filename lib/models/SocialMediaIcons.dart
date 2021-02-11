4f2e4a5bc22a208fd8ecd31cbb0cb4386dfdc4d3
mariocastellanos <maariocastellanos@gmail.com>
2021-02-11 08:46:39 -0800

    Refactored social media icons to reduce clutter, added temporary sshared preferences demo to test method

--- lib/models/SocialMediaIcons.dart
+++ lib/models/SocialMediaIcons.dart
@@ -1,7 +1,7 @@
 import 'package:cta_auto_detail/constants.dart';
+import 'package:cta_auto_detail/screens/sharedPreferences.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
 
 class SocialMediaIcons extends StatelessWidget {
   @override
@@ -11,64 +11,58 @@ class SocialMediaIcons extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: ECCCBlueAccent,
-              shape: BoxShape.circle,
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.black12.withOpacity(.5),
-                    blurRadius: 1,
-                    spreadRadius: 1,
-                    offset: Offset(0, 3))
-              ],
-            ),
-            child: FaIcon(
+          SocialMediaIcon(
+            icon: FaIcon(
               FontAwesomeIcons.facebook,
               color: Colors.black,
               size: 30,
             ),
           ),
-          Container(
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: ECCCBlueAccent,
-              shape: BoxShape.circle,
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.black12.withOpacity(.5),
-                    blurRadius: 1,
-                    spreadRadius: 1,
-                    offset: Offset(0, 3))
-              ],
-            ),
-            child: FaIcon(
+          SocialMediaIcon(
+            icon: FaIcon(
               FontAwesomeIcons.instagram,
               color: Colors.black,
               size: 30,
             ),
           ),
-          Container(
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: ECCCBlueAccent,
-              shape: BoxShape.circle,
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.black12.withOpacity(.5),
-                    blurRadius: 1,
-                    spreadRadius: 1,
-                    offset: Offset(0, 3))
-              ],
-            ),
-            child: FaIcon(
+          SocialMediaIcon(
+            icon: FaIcon(
               FontAwesomeIcons.twitter,
               color: Colors.black,
               size: 30,
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class SocialMediaIcon extends StatelessWidget {
+  final FaIcon icon;
+
+  SocialMediaIcon({this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('icon tapped');
+      },
+      child: Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: ECCCBlueAccent,
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black12.withOpacity(.5),
+                blurRadius: 1,
+                spreadRadius: 1,
+                offset: Offset(0, 3))
+          ],
+        ),
+        child: icon,
       ),
     );
   }
