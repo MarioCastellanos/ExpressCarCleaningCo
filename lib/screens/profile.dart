d413f3f5dedde61e851a9e298af28a05720edc91
mariocastellanos <maariocastellanos@gmail.com>
2021-05-03 13:44:36 -0700

    Removed unused variables display name and adresslist added a stacked widget to display addresses and add addresses

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,6 +1,8 @@
 import 'package:cta_auto_detail/constants.dart';
+import 'package:cta_auto_detail/models/Address_Data.dart';
 import 'package:cta_auto_detail/models/Car_Data.dart';
 import 'package:cta_auto_detail/models/ReusableCard.dart';
+import 'package:cta_auto_detail/models/RoundedButton.dart';
 import 'package:cta_auto_detail/screens/PopUpScreen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -11,8 +13,9 @@ import 'package:flutter/services.dart';
 class ProfileScreen extends StatefulWidget {
   static const String id = 'profileScreen';
   final CarData carData;
+  final AddressData addressData;
 
-  ProfileScreen({this.carData});
+  ProfileScreen({this.carData, this.addressData});
 
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
@@ -20,16 +23,12 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   String userEmail = FirebaseAuth.instance.currentUser.email;
-  String displayName = FirebaseAuth.instance.currentUser.displayName;
-  List<List<String>> addressList;
   int selectedCarIndex = -1;
   int currentIndex;
 
   @override
   void initState() {
     super.initState();
-    addressList = [];
-    addressList.add(['726 Tressy Ave', 'Glendora', 'California', '91740']);
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
@@ -46,58 +45,108 @@ class _ProfileScreenState extends State<ProfileScreen> {
         needsProfileButton: false,
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          CircleAvatarAndTextFields(
-            email: userEmail,
-            onTap: () {
-              print('Needs to be implemented');
-            },
-          ),
-          kSpacerBox,
-          AddressesRow(),
-          kSpacerBox,
-          Expanded(
-            child: Container(
-              color: Colors.transparent,
-              child: HalfScreenCard(
-                childWidget: GridView.builder(
-                  itemCount: widget.carData.carsList.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 1,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            CircleAvatarAndTextFields(
+              email: userEmail,
+              onTap: () {
+                print('Needs to be implemented');
+              },
+            ),
+            Flexible(
+              flex: 1,
+              child: Stack(
+                alignment: Alignment.topRight,
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: ECCCBlueAccent.withOpacity(.5),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: SavedLocationsTextWidget(),
+                        ),
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: widget.addressData.addressCount,
+                            itemBuilder: (context, index) {
+                              return addressRow(index);
+                            },
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                  itemBuilder: (BuildContext context, int index) {
-                    return CarCard(
-                      carIndex: index,
-                      onPressed: () {
-                        selectedCarIndex = index;
-                        if (currentIndex == selectedCarIndex) {
-                          setState(() {
-                            print(index);
-                            currentIndex = -2;
-                            selectedCarIndex = -1;
-                          });
-                        } else {
-                          setState(() {
-                            print(index);
-                            selectedCarIndex = index;
-                            currentIndex = index;
-                          });
-                        }
-                      },
-                      carCardColor:
-                          selectedCarIndex == index ? ECCCBlue : Colors.white,
-                      title: widget.carData.carsList[index].make,
-                    );
-                  },
-                ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                      height: 45,
+                      width: 45,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey.withOpacity(.5),
+                              blurRadius: 4,
+                              spreadRadius: 4,
+                              offset: Offset(0, 4))
+                        ],
+                        shape: BoxShape.circle,
+                        color: ECCCBlue,
+                      ),
+                      child: AddNewAddressButton(),
+                    ),
+                  ),
+                ],
               ),
             ),
-          )
-        ],
+            Expanded(
+              flex: 3,
+              child: Container(
+                color: Colors.transparent,
+                child: HalfScreenCard(
+                  childWidget: GridView.builder(
+                    itemCount: widget.carData.carsList.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 1,
+                    ),
+                    itemBuilder: (BuildContext context, int index) {
+                      return CarCard(
+                        carIndex: index,
+                        onPressed: () {
+                          selectedCarIndex = index;
+                          if (currentIndex == selectedCarIndex) {
+                            setState(() {
+                              print(index);
+                              currentIndex = -2;
+                              selectedCarIndex = -1;
+                            });
+                          } else {
+                            setState(() {
+                              print(index);
+                              selectedCarIndex = index;
+                              currentIndex = index;
+                            });
+                          }
+                        },
+                        carCardColor:
+                            selectedCarIndex == index ? ECCCBlue : Colors.white,
+                        title: widget.carData.carsList[index].make,
+                      );
+                    },
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         splashColor: ECCCBlue,
@@ -124,5 +173,127 @@ class _ProfileScreenState extends State<ProfileScreen> {
         ),
       ),
     );
+  }
+
+  Container addressRow(int index) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          color: Colors.black, borderRadius: BorderRadius.circular(10)),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              child: Icon(
+                Icons.home,
+                color: ECCCBlueAccent,
+                size: 30,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '${widget.addressData.addressList[index].streetAddress}\n'
+                '${widget.addressData.addressList[index].city}\t ${widget.addressData.addressList[index].state}\n'
+                '${widget.addressData.addressList[index].zipCode}',
+                style: TextStyle(
+                  color: ECCCBlueAccent,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AddNewAddressButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          showModalBottomSheet<dynamic>(
+            backgroundColor: Colors.transparent,
+            context: context,
+            builder: (BuildContext context) {
+              return Wrap(
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 20,
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          'Adding Address',
+                          style: TextStyle(
+                            fontSize: 36,
+                            fontFamily: 'Vollkorn',
+                            color: ECCCDarkBlue,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        StreetAddressTF(
+                            // onTAP: (value) {
+                            //   print(value);
+                            // },
+                            ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            CityTF(),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            StateTF(),
+                          ],
+                        ),
+                        kSpacerBox,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ZipCodeTF(),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Expanded(
+                                flex: 1,
+                                child: Container(
+                                  color: Colors.transparent,
+                                )),
+                          ],
+                        ),
+                        kSpacerBox,
+                        ContinueButton(
+                          title: 'Add Address',
+                          textColor: Colors.white,
+                          cBColor: ECCCDarkBlue,
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: Icon(
+          Icons.add_location,
+          color: Colors.white,
+          size: 36,
+        ));
   }
 }
