2a6fe23b511300284627dcb03837f991920045e9
mariocastellanos <maariocastellanos@gmail.com>
2021-10-06 14:53:50 -0700

    Added comments

--- lib/ScreenArguments.dart
+++ lib/ScreenArguments.dart
@@ -1,6 +1,14 @@
-import 'models/DataModels/Car_Data.dart';
+import 'models/DataModels/Car_Data.dart';g
 import 'models/DataModels/CarWashData.dart';
 import 'models/DataModels/Address_Data.dart';
+
+/// AUTHOR : MARIO H.C.H
+/// DATE: 10.5.21
+/// CLASSNAME: SCREENARGUMENTS
+/// PARAMETERS: CarData carData:
+///             AddressData addressData:
+///             CarWashData carWashData:
+/// DESCRIPTION: This class is designed to push data between screens
 
 class ScreenArguments {
   final CarData carData;
